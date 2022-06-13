225da0d4221c270cd85aa8b7422f818763b0973c
dsteins <dsteinschreiber@gmail.com>
2022-06-13 16:09:34 -0500

    New project

---
+++ src/main/java/com/boorbash/boorbashsearchservice/BoorbashSearchServiceApplication.java
@@ -0,0 +1,13 @@
+package com.boorbash.boorbashsearchservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoorbashSearchServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoorbashSearchServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/boorbash/boorbashsearchservice/BoorbashSearchServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boorbash.boorbashsearchservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoorbashSearchServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
