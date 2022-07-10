996023ccd31c6c4342c51133ab7f66f57fe92bca
dsteins <dsteinschreiber@gmail.com>
2022-07-09 21:11:48 -0500

    init-db.sql updated

--- sql/init-db.sql
+++ sql/init-db.sql
@@ -31,30 +31,40 @@ create table menu_entry
     price            numeric,
     dish_pic_url     varchar(1000),
     constraint fk_restaurant_info
-        foreign key(restaurant_id)
-        references restaurant_info(restaurant_id)
-        on delete cascade
+        foreign key (restaurant_id)
+            references restaurant_info (restaurant_id)
+            on delete cascade
 );
 
 insert into menu_entry (restaurant_id, division, dish_name, dish_description, price, dish_pic_url)
-values (
-        (select restaurant_id from restaurant_info where name = 'Little Italy'),
+values ((select restaurant_id from restaurant_info where name = 'Little Italy'),
         'Appetizers',
         'Bruschetta',
         'Tomatoes, basil, mozzarella on perfectly toasted foacaccia bread.',
         12.99,
-        'https://images.unsplash.com/photo-1572695157366-5e585ab2b69f?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMDUzMDJ8MHwxfHNlYXJjaHwzfHxicnVzY2hldHRhfGVufDF8fHx8MTY1NDg5OTI2MA&ixlib=rb-1.2.1&q=80&w=200'
-       ),
-       (
-        (select restaurant_id from restaurant_info where name = 'Danny''s Burgers'),
+        'https://images.unsplash.com/photo-1572695157366-5e585ab2b69f?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMDUzMDJ8MHwxfHNlYXJjaHwzfHxicnVzY2hldHRhfGVufDF8fHx8MTY1NDg5OTI2MA&ixlib=rb-1.2.1&q=80&w=200'),
+       ((select restaurant_id from restaurant_info where name = 'Little Italy'),
+        'Appetizers',
+        'Stuffed Avocados',
+        'delicious avocado stuffed with a delicious mix of delicious stuff making for a delicious choice.',
+        10.99,
+        'https://images.unsplash.com/photo-1546554137-f86b9593a222?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMDUzMDJ8MHwxfHNlYXJjaHw2OHx8Zm9vZHxlbnwxfHx8fDE2NTQ3MzIyNjA&ixlib=rb-1.2.1&q=80&w=200'),
+       ((select restaurant_id from restaurant_info where name = 'Little Italy'),
+        'Main Dishes',
+        'Spaghetti Bolognese',
+        'Spaghetti with red meat sauce.',
+        15.99,
+        'https://images.unsplash.com/photo-1546554137-f86b9593a222?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMDUzMDJ8MHwxfHNlYXJjaHw2OHx8Zm9vZHxlbnwxfHx8fDE2NTQ3MzIyNjA&ixlib=rb-1.2.1&q=80&w=200'),
+       ((select restaurant_id from restaurant_info where name = 'Danny''s Burgers'),
         'Burgers',
         'Mushroom Burger Deluxe',
         'A juicy, tasty, greasy load of a burger',
         15.00,
-        'https://images.unsplash.com/photo-1551987840-f62d9c74ae78?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMDUzMDJ8MHwxfHNlYXJjaHwxfHxtdXNocm9vbSUyMGJ1cmdlcnxlbnwxfHx8fDE2NTcxNTI3NDA&ixlib=rb-1.2.1&q=80&w=200'
-       );
+        'https://images.unsplash.com/photo-1551987840-f62d9c74ae78?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMDUzMDJ8MHwxfHNlYXJjaHwxfHxtdXNocm9vbSUyMGJ1cmdlcnxlbnwxfHx8fDE2NTcxNTI3NDA&ixlib=rb-1.2.1&q=80&w=200');
 commit;
 
-select * from menu_entry;
+select *
+from menu_entry;`
+
 
 
